15057749e417f1fd104ce9efd84c860c3afa8cbe
Ayush Sunariya <ayushprajapati47@gmail.com>
2026-06-21 19:47:08 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    void solve(vector<string>& ans, string res, int n, int c, int o) {
+        if(o==n && c==n){
+            ans.push_back(res);
+            return;
+        }
+        if (o < n) {
+            solve(ans,res+'(',n,c,o+1);
+        }
+        if(c<o && c<n){
+            solve(ans,res+')',n,c+1,o);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string res;
+        int c = 0, o = 0;
+        solve(ans, res, n, c, o);
+        return ans;
+    }
+};
