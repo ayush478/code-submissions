86dc61051f9cd02d6294d7813d97c06c8ec08412
Ayush Sunariya <ayushprajapati47@gmail.com>
2026-08-01 00:19:25 +0530

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.cpp
@@ -0,0 +1,19 @@
+class KthLargest {
+   public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int K;
+    KthLargest(int k, vector<int>& nums) {
+        this->K = k;
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > K) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
