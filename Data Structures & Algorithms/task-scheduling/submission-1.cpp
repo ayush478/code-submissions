e510b538ff66cba41eb2c8b0090d31ec34f36e26
Ayush Sunariya <ayushprajapati47@gmail.com>
2026-09-06 01:36:59 +0530

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+   public:
+    int leastInterval(vector<char>& tasks, int n) {
+        priority_queue<int> pq;
+        unordered_map<char, int> mp;
+
+        for (auto& x : tasks) {
+            mp[x]++;
+        }
+        for (auto& x : mp) {
+            pq.push(x.second);
+        }
+
+        int ans = 0;
+        while (!pq.empty()) {
+            int time = 0;
+            vector<int> temp;
+            int cycle = n + 1;
+            for (int i = 0; i < cycle; i++) {
+                if (!pq.empty()) {
+                    temp.push_back(pq.top());
+                    pq.pop();
+                    time++;
+                }
+            }
+            for (int count : temp) {
+                if (--count > 0) pq.push(count);
+            }
+            ans += !pq.empty() ? cycle : time;
+        }
+        return ans;
+    }
+};
