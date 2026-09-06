e463aaebc10c38211ffe453a09e1bf1a668012a5
Ayush Sunariya <ayushprajapati47@gmail.com>
2026-09-06 12:59:17 +0530

    Add: design-twitter-feed - submission-2

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-2.cpp
@@ -0,0 +1,38 @@
+class Twitter {
+   public:
+    int timestamp;
+    unordered_map<int, unordered_set<int>> following;
+    unordered_map<int, vector<pair<int, int>>> userTweets;
+
+    Twitter() { timestamp = 0; }
+
+    void postTweet(int userId, int tweetId) {
+        userTweets[userId].push_back({timestamp++, tweetId});
+    }
+
+    vector<int> getNewsFeed(int userId) {
+        priority_queue<pair<int, int>> pq;
+        for (const auto& tweet : userTweets[userId]) {
+            pq.push(tweet);
+        }
+        for (int followeeId : following[userId]) {
+            for (const auto& tweet : userTweets[followeeId]) {
+                pq.push(tweet);
+            }
+        }
+        vector<int> res;
+        while (!pq.empty() && res.size() < 10) {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+
+    void follow(int followerId, int followeeId) {
+        if (followerId != followeeId) {
+            following[followerId].insert(followeeId);
+        }
+    }
+
+    void unfollow(int followerId, int followeeId) { following[followerId].erase(followeeId); }
+};
